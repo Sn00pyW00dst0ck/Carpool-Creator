38a6583de2456b1dfd7f93335638cf4bfff297e5
Sn00pyW00dst0ck <gabealdous@gmail.com>
2023-08-22 17:22:29 -0400

    Fixing lint issue

--- test/test_match_riders_randomly.py
+++ test/test_match_riders_randomly.py
@@ -1,7 +1,7 @@
 #pylint: disable=C0103
 """Module providing match_riders_randomly function unit tests"""
 
-# pylint: disable=C0413, E0401
+# pylint: disable=C0413, E0401, W0611
 import unittest
 from unittest.mock import patch
 from io import StringIO
@@ -12,7 +12,7 @@ import os.path
 src_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/src/')
 sys.path.append(src_dir)
 from main import match_riders_randomly
-# pylint: enable=C0413, E0401
+# pylint: enable=C0413, E0401, W0611
 
 class MatchRidersRandomlyUnitTests(unittest.TestCase):
     """Class containing unit tests for the match_riders_randomly function"""
--- test/test_read_carpool_data.py
+++ test/test_read_carpool_data.py
@@ -1,7 +1,7 @@
 #pylint: disable=C0103
 """Module providing read_carpool_data function unit tests"""
 
-# pylint: disable=C0413, E0401
+# pylint: disable=C0413, E0401, W0611
 import unittest
 from unittest.mock import patch
 from io import StringIO
@@ -12,7 +12,7 @@ import os.path
 src_dir = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/src/')
 sys.path.append(src_dir)
 from main import read_carpool_data
-# pylint: enable=C0413, E0401
+# pylint: enable=C0413, E0401, W0611
 
 class ReadCarpoolDataUnitTests(unittest.TestCase):
     """Class containing unit tests for the match_riders_randomly function"""
